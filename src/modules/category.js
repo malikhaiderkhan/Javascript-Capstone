1c806a0fdcbeeed6ea35d0aac318cecb889e5acc
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-07-14 17:33:08 +0100

    added counter

--- src/modules/category.js
+++ src/modules/category.js
@@ -4,38 +4,13 @@ const categoryData = (data) => {
     data.forEach(list => {
         const CAT_LIST = document.createElement('div');
         CAT_LIST.classList.add('cat-list');
-        CAT_LIST.innerHTML = `${list}`;
+        CAT_LIST.innerHTML = `${list} ${list.length}`;
 
         container.appendChild(CAT_LIST);
 
     });
      
   }
-
- 
-
-
-  fetch('https://fakestoreapi.com/products/category/jewelery')
-  .then(response => response.json())
-  .then(data => {
-    // Display the number of products in the console
-    const JEWELERY = data.length;
-    window.JEWELERY = JEWELERY;
-  })
-  .catch(error => {
-    throw new Error('An error occurred:');
-  });
-
-  fetch('https://fakestoreapi.com/products/category/electronics')
-  .then(response => response.json())
-  .then(data => {
-    // Display the number of products in the console
-    const ELECTRONICS = data.length;
-    window.ELECTRONICS = ELECTRONICS;
-  })
-  .catch(error => {
-    throw new Error('An error occurred:');
-  });
 
 export default categoryData;
 
