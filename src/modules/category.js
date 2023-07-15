699716bd347bd17aed7126e053a914a8bb20d393
Haider <malikhaiderkhan81@gmail.com>
2023-07-15 20:24:25 +0500

    fix linter error

--- src/modules/category.js
+++ src/modules/category.js
@@ -1,15 +1,15 @@
-const container = document.querySelector('.category');
-
 const categoryData = (data, container) => {
   if (!container) {
     container = document.querySelector('.category'); // Use default container if not provided
   }
-  container.innerHTML = '';
 
   const totalItems = data.flat().length;
   const totalCountElement = document.createElement('div');
   totalCountElement.classList.add('total-count');
   totalCountElement.textContent = `Total Items: ${totalItems}`;
+
+  // Clear the container before adding the total count element
+  container.innerHTML = '';
   container.appendChild(totalCountElement);
 };
 
--- src/test/categoryCounter.test.js
+++ src/test/categoryCounter.test.js
@@ -1,18 +1,8 @@
 import categoryData from '../modules/category.js';
 
 describe('categoryData', () => {
-  let container;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    container.classList.add('category');
-  });
-
-  afterEach(() => {
-    container = null;
-  });
-
   test('should display the total count of items', () => {
+    const container = document.createElement('div');
     const data = [['Item 1', 'Item 2'], ['Item 3']];
     categoryData(data, container);
 
@@ -22,6 +12,7 @@ describe('categoryData', () => {
   });
 
   test('should display the correct total count of items when data is empty', () => {
+    const container = document.createElement('div');
     const data = [];
     categoryData(data, container);
 
@@ -31,6 +22,7 @@ describe('categoryData', () => {
   });
 
   test('should display the correct total count of items when data has nested arrays', () => {
+    const container = document.createElement('div');
     const data = [['Item 1', 'Item 2'], ['Item 3', 'Item 4', 'Item 5'], ['Item 6']];
     categoryData(data, container);
 
