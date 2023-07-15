e1fbdabe194f73b8004645ecd496817019591840
Sulaimon Azeez Ayodeji <sulaimonaa@gmail.com>
2023-07-15 08:48:21 +0100

    add counter test

---
+++ src/test/categoryCounter.test.js
@@ -0,0 +1,26 @@
+import categoryData from '../modules/category.js';
+
+describe('categoryData', () => {
+  it('should display the length of each list', () => {
+    // Create a mock container element
+    const container = document.createElement('div');
+    container.classList.add('category');
+
+    // Define sample data
+    const data = ['List 1', 'List 2', 'List 3'];
+
+    // Call the categoryData method
+    categoryData(data);
+
+    // Verify the correct number of cat-list elements are created
+    const catListElements = container.querySelectorAll('.cat-list');
+    expect(catListElements.length).toBe(data.length);
+
+    // Verify that each cat-list element displays the list name and its length
+    catListElements.forEach((element, index) => {
+      const listName = data[index];
+      const expectedText = `${listName} ${listName.length}`;
+      expect(element.textContent).toBe(expectedText);
+    });
+  });
+});
