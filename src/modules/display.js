8c71421b5e80b929476ebc151fee10e4ef46f75b
Haider <malikhaiderkhan81@gmail.com>
2023-07-15 19:27:31 +0500

    fix linter errors

--- src/modules/display.js
+++ src/modules/display.js
@@ -12,37 +12,41 @@ const productContainer = document.querySelector('.products');
 
 const display = async (data) => {
   productContainer.innerHTML = '';
-  try {
-    for (const list of data) {
-      const productWrap = document.createElement('div');
-      productWrap.classList.add('card');
-      productWrap.innerHTML = `
-        <div class='card-img'><img src='${list.image}' /></div>
-        <div class='card-title'><h4>${list.title}</h4></div>
-        <div class='card-price'><h5>$${list.price}</h5></div>
-        <div class='card-like'><img src='${likes}' class='likes' /> <span id='likes-${list.id}'>...</span> likes</div>
-        <button class='btn btn-comment' data-url='https://fakestoreapi.com/products/${list.id}'>Comments <img src='${commentbtn}' /></button>
-        <button class='btn'>Check Item</button>
-      `;
 
-      productContainer.appendChild(productWrap);
+  data.forEach(async (list) => {
+    const productWrap = document.createElement('div');
+    productWrap.classList.add('card');
+    productWrap.innerHTML = `
+            <div class='card-img'><img src='${list.image}' /></div>
+            <div class='card-title'><h4>${list.title}</h4></div>
+            <div class='card-price'><h5>$${list.price}</h5></div>
+            <div class='card-like'><img src='${likes}' class='likes' /> <span id='likes-${list.id}'>...</span> likes</div>
+            <button class='btn btn-comment' data-url='https://fakestoreapi.com/products/${list.id}'>Comments <img src='${commentbtn}' /></button>
+            <button class='btn'>Check Item</button>
+        `;
 
-      const heartIcon = productWrap.querySelector('.likes');
+    productContainer.appendChild(productWrap);
 
-      // Fetch the number of likes from the API
-      const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/myodB0I2hWMN7rQnMtyn/likes/');
-      const likesData = await response.json();
+    const heartIcon = productWrap.querySelector('.likes');
+
+    try {
+      const likesResponse = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/myodB0I2hWMN7rQnMtyn/likes/');
+      const likesData = await likesResponse.json();
 
       const likesElement = document.getElementById(`likes-${list.id}`);
-      const like = likesData.find((el) => el.item_id === list.id);
+      const likesInfo = likesData.find((el) => el.item_id === list.id);
 
-      if (like) {
-        likesElement.textContent = like.likes;
+      if (likesInfo) {
+        likesElement.textContent = likesInfo.likes;
       }
+    } catch (error) {
+      throw new Error('Error fetching likes:', error);
+    }
 
-      heartIcon.addEventListener('click', async (e) => {
-        e.stopPropagation();
+    heartIcon.addEventListener('click', async (e) => {
+      e.stopPropagation();
 
+      try {
         const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/myodB0I2hWMN7rQnMtyn/likes/';
         await fetch(url, {
           method: 'POST',
@@ -54,12 +58,14 @@ const display = async (data) => {
           }),
         });
 
-        likesElement.textContent = parseInt(likesElement.textContent, 10) + 1;
-      });
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
+        const likesElement = document.getElementById(`likes-${list.id}`);
+        const currentLikes = parseInt(likesElement.textContent, 10);
+        likesElement.textContent = currentLikes + 1;
+      } catch (error) {
+        throw new Error('Error updating likes:', error);
+      }
+    });
+  });
 };
 
 export default display;
